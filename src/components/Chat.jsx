43ba71fbe85f0535c4e17f363deabf0c4bd51384
AkshayTishwan <146157295+AkshayTishwan@users.noreply.github.com>
2024-02-27 19:25:08 +0530

    Update Chat.jsx

--- src/components/Chat.jsx
+++ src/components/Chat.jsx
@@ -1,28 +1,35 @@
-import React, { useContext } from 'react'
-import Cam from "../images/cam.png"
-import Add from "../images/add.png"
-import More from "../images/more.png"
-import Messages from './Messages'
-import Input from "./Input"
-import { ChatContext } from '../context/ChatContext'
+import React, { useContext } from "react";
+import Cam from "../images/cam.png";
+import Add from "../images/add.png";
+import More from "../images/more.png";
+import Messages from "./Messages";
+import Input from "./Input";
+import { ChatContext } from "../context/ChatContext";
+
+const capitalizeFirstLetter = (string) => {
+  if (typeof string !== "string" || string.length === 0) {
+    return "";
+  }
+  return string.charAt(0).toUpperCase() + string.slice(1);
+};
 
 const Chat = () => {
-
   const { data } = useContext(ChatContext);
 
   return (
-    <div className='chat'>
+    <div className="chat">
       <div className="chatInfo">
-        <span>{data.user.displayName}</span>
+        <span>{capitalizeFirstLetter(data.user.displayName)}</span>
         <div className="chatIcons">
           <img src={Cam} alt="" />
           <img src={Add} alt="" />
           <img src={More} alt="" />
         </div>
       </div>
-      <Messages/>
-      <Input/>
+      <Messages />
+      <Input />
     </div>
-  )
-}
-export default Chat
+  );
+};
+
+export default Chat;
